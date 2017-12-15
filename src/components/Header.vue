aeaf069e5395e260a1806642e864f2578ca39423
djstojadin <stojkovicdesign@gmail.com>
2017-12-15 13:10:51 +0100

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <div class="stats">
+      <ul>
+        <li>Cryptocurrencies: </li>
+        <li>Markets: </li>
+      </ul>
+      <ul>
+        <li>Market Cap: </li>
+        <li>24h Vol: </li>
+        <li>BTC Dominance: </li>
+      </ul>
+      <ul>
+        <li>english</li>
+        <li>USD</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+  .header {
+    max-width: 90%;
+    margin: 0 auto;
+  }
+</style>
+
