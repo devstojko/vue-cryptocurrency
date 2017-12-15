00d95d0bc02cfe5a9b509dd5336f6e6ad19a4d35
djstojadin <stojkovicdesign@gmail.com>
2017-12-15 14:45:50 +0100

    fetch api data

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="app">
-    <app-header></app-header>
+    <app-header :headerData="headerData"></app-header>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
   import Header from '@/components/Header';
 
 export default {
   name: 'app',
   components: {
     'app-header': Header
+  },
+  data() {
+    return {
+      headerData: {},
+      tickerData: []
+    }
+  },
+  computed: {
+    getGlobalData() {
+      return axios.get('https://api.coinmarketcap.com/v1/global/');
+    },
+    getTickerList() {
+      return axios.get('https://api.coinmarketcap.com/v1/ticker/');
+    },
+
+  },
+  created() {
+    axios.all([this.getGlobalData, this.getTickerList]).then(axios.spread((global, ticker) => {
+     
+      this.headerData = global.data;
+      this.tickerData = ticker.data;
+
+    }))
   }
 };
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,25 +2,41 @@
   <div class="header">
     <div class="stats">
       <ul>
-        <li>Cryptocurrencies: </li>
-        <li>Markets: </li>
+        <li>Cryptocurrencies: {{ headerData.active_currencies }}</li>
+        <li>Markets: {{ headerData.active_markets }} </li>
       </ul>
       <ul>
-        <li>Market Cap: </li>
-        <li>24h Vol: </li>
-        <li>BTC Dominance: </li>
+        <li>Market Cap: {{ headerData.total_market_cap_usd | formatPrice }}</li>
+        <li>24h Vol: {{ headerData.total_24h_volume_usd | formatPrice }}</li>
+        <li>BTC Dominance: {{ headerData.bitcoin_percentage_of_market_cap | formatPercentage }}</li>
       </ul>
       <ul>
         <li>english</li>
         <li>USD</li>
+        <li>Last update: {{ headerData.last_updated | fromNow }}</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+ import moment from 'moment';
+ import numeral from 'numeral';
+
 export default {
-  name: "Header"
+  name: 'Header',
+  props: ['headerData'],
+  filters: {
+    'fromNow'(val) {
+      return moment.unix(val).fromNow();
+    },
+    'formatPrice'(val) {
+      return numeral(val).format('$0,0');
+    },
+    'formatPercentage'(val) {
+      return val === undefined ? '0%' : val;
+    }
+  }
 }
 </script>
 
